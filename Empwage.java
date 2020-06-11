da08d9b8a6145f176ef98fbaeb03f04f50b3d393
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-11 17:20:23 +0530

    ADDED: Daily Wage functionality

--- Empwage.java
+++ Empwage.java
@@ -2,7 +2,11 @@ public class Empwage
 {
 
 	double empCheck=0.0;
+	int dailyWage=0;
+
 	private static final int IS_FULL_TIME = 1;
+	private static final int WAGE_PER_HOUR=20;
+	private static final int HOURS_PER_DAY=8;
 
    public void getWelcomeMessage()
    {
@@ -13,7 +17,12 @@ public class Empwage
 	{
 		empCheck = Math.floor(Math.random() * 10) % 2;
 		if ( empCheck == IS_FULL_TIME)
+		{
 			System.out.println("Employee is Present");
+			dailyWage = (WAGE_PER_HOUR * HOURS_PER_DAY);
+			System.out.println("Daily wage would be = Rs." + dailyWage);
+		}
+
 		else
 			System.out.println("Employee is Absent");
 	}
