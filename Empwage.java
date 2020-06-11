4885a476b53a1eac995e651f7318ce1ea5b36995
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-06-11 17:04:01 +0530

    ADDED: Welcome message

---
+++ Empwage.java
@@ -0,0 +1,15 @@
+public class Empwage
+{
+   public void getWelcomeMessage()
+   {
+      System.out.println("Welcome To Employee Wage Computation Process");
+   }
+   
+   public static void main(String[] args)
+   {
+      Empwage emp = new Empwage();
+      emp.getWelcomeMessage();
+   }
+   
+}
+
